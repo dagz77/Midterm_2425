2f0e3a7f05f13a36bed31fcb1e125e82c2d986f5
dagz77 <20237877@s.ubaguio.edu>
2024-10-18 13:08:28 +0800

    committed

--- Bastawang_AP/src/app/layout/layout.component.ts
+++ Bastawang_AP/src/app/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { ContentComponent } from '../content/content.component';
+import { UserDetailsComponent } from "../user-details/user-details.component";
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, ContentComponent, FooterComponent],
+  imports: [HeaderComponent, ContentComponent, FooterComponent, UserDetailsComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
